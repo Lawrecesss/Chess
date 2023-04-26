10c9004506479c6e8172cc2d0be2a39220a0c500
Lawrence <lhshein14@gmail.com>
2023-04-26 19:36:10 +0800

    File Uploaded

---
+++ Chess_App-master/Assets/Menu/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitButton()
+    {
+        Application.Quit();
+        Debug.Log("Player has Quit");
+    }
+}
